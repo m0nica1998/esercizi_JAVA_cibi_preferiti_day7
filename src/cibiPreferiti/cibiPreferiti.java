d44d0dc5b945c9f905a28fa33b6ad1da8cb0f3a6
m0nica1998 <monica.cesario98@icloud.com>
2025-03-31 21:14:26 +0200

    esercizio con array

--- src/cibiPreferiti/cibiPreferiti.java
+++ src/cibiPreferiti/cibiPreferiti.java
@@ -2,6 +2,31 @@ package cibiPreferiti;
 
 public class cibiPreferiti {
  public static void main(String[] args) {
+	 /*"pizza", "sushi", "lasagna", "insalata", "pane"
+ 
+stampa a schermo  la lunghezza della lista di cibi preferiti
+ 
+mostra a schermo il cibo in prima posizione
+ 
+mostra a schermo il cibo all'ultima posizione
+ 
+mostra a schermo il cibo che sta al centro
+  */
+	 
+	 String[] cibiPreferiti = {"pizza", "sushi", "lasagna", "insalata", "pane"};
+	 System.out.println(cibiPreferiti.length);
+	 System.out.println(cibiPreferiti[0]);
+	 System.out.println(cibiPreferiti[(cibiPreferiti.length)-1]);
+	 if((cibiPreferiti.length) % 2 == 0) {
+		 
+		 System.out.println("Non esiste un elemento centrale");
+	 } else {
+		 int n = (cibiPreferiti.length - 1);
+		 int m = (n / 2);
+		 System.out.println(n + " " + m);
+		int indiceElemCentrale = m;
+		System.out.println(cibiPreferiti[indiceElemCentrale]);
+	 }
 	 
  }
 }
